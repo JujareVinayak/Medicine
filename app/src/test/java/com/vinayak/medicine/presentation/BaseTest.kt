7ec7c9c2106b1f21be7f02897b477f7244074b1c
Jujare Vinayak <jujare.vinayak@lowes.com>
2022-09-14 19:38:23 +0530

    Fixed dev comments.

--- app/src/test/java/com/vinayak/medicine/presentation/BaseTest.kt
+++ app/src/test/java/com/vinayak/medicine/presentation/BaseTest.kt
@@ -1,26 +1,8 @@
-/*
-*Lowe's Companies Inc., Android Application
-* Copyright (C)  Lowe's Companies Inc.
-*
-*  The Lowe's Application is the private property of
-*  Lowe's Companies Inc. Any distribution of this software
-*  is unlawful and prohibited.
-*/
 package com.vinayak.medicine.presentation
 
-import com.google.gson.Gson
 import org.junit.Before
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations.openMocks
 
-/**
- * Created by Raghavendra D G on 19/03/20.
- *
- * Copyright (C) 2020 Lowe's Companies Inc.
- *
- *  This application is the private property of Lowe's Companies Inc.
- *  Any distribution of this software is unlawful and prohibited.
- */
 open class BaseTest {
 
     @Before
